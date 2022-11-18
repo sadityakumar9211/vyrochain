49234ee590715884bcb3d990ab250aaee63be71c
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-11-19 03:43:47 +0530

    MODIFY: helper-hardhat-config.js

--- medichain-hardhat/helper-hardhat-config.js
+++ medichain-hardhat/helper-hardhat-config.js
@@ -25,10 +25,23 @@ const CATEGORY = 0
 const IPFS_HASH = ""       //The IPFS hash generated after uploading the file of the patient to the pinata IPFS node. 
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
+// Governor Values
+export const QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+export const MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
+// export const VOTING_PERIOD = 45818 // 1 week - how long the vote lasts. This is pretty long even for local tests
+export const VOTING_PERIOD = 5; // blocks
+export const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+
 module.exports = {
     networkConfig,
     developmentChains,
     CATEGORY,
     IPFS_HASH,
     VERIFICATION_BLOCK_CONFIRMATIONS,
+    QUORUM_PERCENTAGE,
+    MIN_DELAY,
+    VOTING_DELAY, 
+    VOTING_PERIOD,
+    ADDRESS_ZERO,
 }
