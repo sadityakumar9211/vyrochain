9835aba7839d30d62ff56446bd53e6e865537962
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-11-19 03:41:40 +0530

    ADD: DAO contracts

---
+++ medichain-hardhat/contracts/governance_standard/TimeLockControl.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract  TimeLock is TimelockController{
+    // minDelay: you gotta wait for this duration at least before executing the proposed changes. 
+    // proposers: list of addresses that are gonna be able to propose any changes. 
+    // executers: who can execute when a proposal goes through
+    constructor(
+        uint256 minDelay, 
+        address [] memory proposers,
+        address [] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
