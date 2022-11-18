cef59833593c650ee18807af2288ee920c753cd4
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-11-19 03:43:09 +0530

    ADD: deploy script for governor-contract

---
+++ medichain-hardhat/deploy/03-deploy-governor-contract.js
@@ -0,0 +1,42 @@
+const {network} = require('hardhat')
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const {
+  networkConfig,
+  developmentChains,
+  VOTING_DELAY,
+  VOTING_PERIOD,
+  QUORUM_PERCENTAGE,
+} = require('../helper-hardhat-config')
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deployer } = await getNamedAccounts()
+  const { deploy, log, get } = deployments
+
+  const governanceToken = await get('GovernanceToken')
+  const timeLock = await get('TimeLock')
+  const args = [
+    governanceToken.address,
+    timeLock.address,
+    VOTING_DELAY,
+    VOTING_PERIOD,
+    QUORUM_PERCENTAGE,
+  ]
+  log('--------------------------------------')
+  log('Deploying Governor Contract...')
+  const governorContract = await deploy('GovernorContract', {
+    from: deployer,
+    args,
+    log: true,
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  })
+  log(`Governor Contract is deployed at address ${governorContract.address}`)
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(governorContract.address, args)
+  }
+}
+
