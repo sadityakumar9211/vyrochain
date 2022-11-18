0527894168e5a7e2acb14209e29d922f502be480
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-11-19 03:42:54 +0530

    ADD: deploy script for timelock

---
+++ medichain-hardhat/deploy/02-deploy-timelock.js
@@ -0,0 +1,28 @@
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const { MIN_DELAY }  = require('../helper-hardhat-config')
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  log('----------------------------------------')
+  log('Deploying Time Lock........')
+  const timeLock = await deploy('TimeLock', {
+    from: deployer,
+    args: [MIN_DELAY, [], []],
+    log: true,
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(timeLock.address, [])
+  }
+
+  log(`Deployed Governance token at address ${timeLock.address}`)
+}
