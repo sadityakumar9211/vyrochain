9f56573406cbb1d8751609ac2ae0678f16da54fc
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-11-19 07:13:13 +0530

    MODIFY: helper-hardhat-config.js

--- medichain-hardhat/helper-hardhat-config.js
+++ medichain-hardhat/helper-hardhat-config.js
@@ -22,17 +22,24 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const CATEGORY = 0
-const IPFS_HASH = ""       //The IPFS hash generated after uploading the file of the patient to the pinata IPFS node. 
+// const IPFS_HASH = ""       //The IPFS hash generated after uploading the file of the patient to the pinata IPFS node. 
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
+const proposalsFile = "proposals.json";
 // Governor Values
-export const QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
-export const MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
+const QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+const MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
 // export const VOTING_PERIOD = 45818 // 1 week - how long the vote lasts. This is pretty long even for local tests
-export const VOTING_PERIOD = 5; // blocks
-export const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
-export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+const VOTING_PERIOD = 5; // blocks
+const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
+const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
+const NEW_Chairperson_INFO = {
+    address: ADDRESS_ZERO,
+}
+const FUNC = "changeChairperson";
+const PROPOSAL_DESCRIPTION = `Proposal @1: Make the new Chairperson to Aditya with address ${ADDRESS_ZERO}`
+const PROPOSALS_FILE = path.join(__dirname, "proposals.json")
 module.exports = {
     networkConfig,
     developmentChains,
@@ -44,4 +51,8 @@ module.exports = {
     VOTING_DELAY, 
     VOTING_PERIOD,
     ADDRESS_ZERO,
+    NEW_Chairperson_INFO,
+    FUNC, 
+    PROPOSAL_DESCRIPTION,
+    PROPOSALS_FILE,
 }
